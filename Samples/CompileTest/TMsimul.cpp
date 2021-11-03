ee3d39e16500ce87edc9bea955990fe657a348b1
Räty <jonijura@jyu.fi>
2021-11-03 15:10:40 +0200

    s-m reunaehto beta versio

--- Samples/CompileTest/TMsimul.cpp
+++ Samples/CompileTest/TMsimul.cpp
@@ -30,7 +30,7 @@ using namespace std;
 using namespace gfd;
 
 const uint space_steps = 5; // number of space elements per unit length
-const uint time_steps = 1000; // number of time steps
+const uint time_steps = 4000; // number of time steps
 const double dt = 0.003; //time step size
 
 double getDouble(const std::string &str, const double def) {
@@ -70,41 +70,59 @@ Buffer<uint> getUints(const std::string &str) {
 }
 
 void line(){
-	cout << "---------------"<<endl;
+	std::cout << "---------------"<<endl;
 }
-void loadMesh(PartMesh &mesh, string file){
+void loadMesh(PartMesh &mesh, string file, Buffer<double> &boundaryNodes){
     Text input;
-    if(input.load(file)) cout << "mesh file loaded succesfully." << endl;
+    if(input.load(file)) std::cout << "mesh file loaded succesfully." << endl;
     while(input.hasRow()) {
 		if(input.getRow().substr(0, 6).compare("$Nodes") == 0){
-            cout << "reading nodes." << endl;
+            std::cout << "reading nodes." << endl;
             break;
         } 
 	}
     const uint numVert=getUint(input.getRow(),0);
-    cout << numVert << endl;
+    std::cout << numVert << endl;
     mesh.resizeNodeBuffer(numVert);
 	for(uint i=0; i<numVert; i++) {
 		const Buffer<double> pos = getDoubles(input.getRow());
-		mesh.addNode(Vector4(pos[0], (pos.size() > 1 ? pos[1] : 0.0), 
-													(pos.size() > 2 ? pos[2] : 0.0), 0.0));
+		mesh.addNode(Vector4(pos[0], pos[1], 0.0, 0.0));
 	}
-    if(input.getRow().substr(0, 9).compare("$EndNodes") == 0) cout << "Nodes read succesfully." << endl;
+    if(input.getRow().substr(0, 9).compare("$EndNodes") == 0) std::cout << "Nodes read succesfully." << endl;
 	if(input.getRow().substr(0, 6).compare("$Faces") == 0){
-        cout << "reading faces and edges." << endl;
+        std::cout << "reading faces and edges." << endl;
     }
     const uint numFace=getUint(input.getRow(),0);
+	Buffer<uint> flags(numFace*4);//record edges that are linked to only one face -> boundary edges
+	flags.fill(0);
     for(uint i=0; i<numFace; i++) {
 		const Buffer<uint> face = getUints(input.getRow());
 		Buffer<uint> edge(face.size());
 		for(uint i=0; i<face.size(); i++){
 			edge[i] = mesh.addEdge(face[i%face.size()]-1, face[(i+1)%face.size()]-1);
+			flags[edge[i]]++;
 		}
 		mesh.addFace(edge);
 	}
-    cout << numFace << endl;
-    //read faces and edges
-    if(input.getRow().substr(0, 9).compare("$EndFaces") == 0) cout << "Faces and edges read succesfully." << endl;
+	flags.resize(mesh.getEdgeSize());
+	for(uint i=0; i<flags.size();i++){
+		flags[i]=flags[i]%2;//flags[i]=1 iff edge number i is on the boundary
+	}
+	//for(uint i=0;i<flags.size();i++) cout << flags[i] << " ";
+	Buffer<double> flagsV(mesh.getNodeSize());
+	flagsV.fill(0.0);
+	for(uint i=0; i<flags.size();i++){
+		if(flags[i]==1){//flags[i]=1 iff node number i is on the boundary
+			flagsV[mesh.getEdgeNodes(i)[0]]=1.0;
+			flagsV[mesh.getEdgeNodes(i)[1]]=1.0;
+			//mesh.setNodeFlag(mesh.getEdgeNodes(i)[0],1);
+			//mesh.setNodeFlag(mesh.getEdgeNodes(i)[1],1);
+		}
+	}
+	//for(uint i=0;i<flagsV.size();i++) cout << flagsV[i] << " ";
+	boundaryNodes=flagsV;
+    std::cout << numFace << endl;
+    if(input.getRow().substr(0, 9).compare("$EndFaces") == 0) std::cout << "Faces and edges read succesfully." << endl;
 }
 
 int main(int argc, const char* argv[]) {
@@ -113,6 +131,7 @@ int main(int argc, const char* argv[]) {
 	auto starttime = chrono::system_clock::now();
 
     PartMesh mesh(0, 1, 2);
+	Buffer<double> boundaryNodes;
     if(argc==1){
         BuilderMesh bmesh(2);
         const double dx = 1.0 / double(space_steps);
@@ -120,10 +139,14 @@ int main(int argc, const char* argv[]) {
         mesh.swap(bmesh);
     }
     else{
-        loadMesh(mesh,argv[1]);
+        loadMesh(mesh,argv[1],boundaryNodes);
     }
+	Text stat;
+	mesh.writeStatistics(stat);
+	stat.save("stats.txt");
     Dec dec(mesh, 0, mesh.getDimension());
 
+	Diagonal<double> P1(boundaryNodes,0.0);
     Derivative d0; 
     d0=dec.integrateDerivative(fg_prim0, d0);
     Diagonal<double> h1;
@@ -138,28 +161,27 @@ int main(int argc, const char* argv[]) {
 	delta.setScale(dt, h0i*transpose(d0));
 	Sparse<double> gamma;
 	gamma.setScale(dt, h1*d0);
-	double sol44[time_steps];
-	double sol70[time_steps];
+	P1.setScale(0.1*dt,h0i*P1);
+	double solt[time_steps];
 
-	cout << "Mesh and derivates generated in " << chrono::duration<double>(chrono::system_clock::now() - starttime).count() << " seconds" << endl;
+	std::cout << "Mesh and derivates generated in " << chrono::duration<double>(chrono::system_clock::now() - starttime).count() << " seconds" << endl;
 	starttime = chrono::system_clock::now();
-
+	uint kp = mesh.getNodeSize()/2;
     for(double i=0; i<time_steps; i++) {
-		e+=delta*h;
-        e.m_val[1700]=sin(PIx2*i/300.0);//60 158
+		e+=delta*h;//-P1*e
+        e.m_val[kp]=sin(PIx2*i/650.0);//60 158
         h-=gamma*e;
-		sol44[int(i+0.1)]=e.m_val[7505];//36 44
-		sol70[int(i+0.1)]=e.m_val[4505];//24 70
+		solt[int(i+0.1)]=e.m_val[kp-5];//36 44
     }
 	
-	cout << "Timeintegration performed in " << chrono::duration<double>(chrono::system_clock::now() - starttime).count() << " seconds" << endl;
+	std::cout << "Timeintegration performed in " << chrono::duration<double>(chrono::system_clock::now() - starttime).count() << " seconds" << endl;
 	starttime = chrono::system_clock::now();
 
 	ofstream myfile;
 	myfile.open ("solutions.txt");
 	for(uint i=0;i<time_steps;i++){
-		myfile << sol44[i] << " " << sol70[i] << endl;
+		myfile << solt[i] << endl;
 	}
 	myfile.close();
-    finalizeMPI();
+    gfd::finalizeMPI();
 }
