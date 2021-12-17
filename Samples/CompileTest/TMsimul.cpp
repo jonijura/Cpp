e3d9424e5e169d6b8827bbac8df2104b6053c034
Räty <jonijura@jyu.fi>
2021-12-17 14:33:13 +0200

    lisätty kommentteja

--- Samples/CompileTest/TMsimul.cpp
+++ Samples/CompileTest/TMsimul.cpp
@@ -1,22 +1,16 @@
-/*Yritetään toistaa pyDEC:illä tuotettuja simulaatiotuloksia
+/*
+Raportin DEC simulaatioihin käytetty koodi
 
-MPI ei toimi, tekee saman homman useammassa prosessissa. muuta kääntäjäasetuksia?
-Kuinka voin debugatessa tarkastella bufferin lukuja ilman segventation faulttia?
+Käyttö: syötä komentoriviltä käynnistyksen perään verkon tiedosto
+ja halutessa muuttuja space_steps kokonaislukuna. Säteen tai permittiivisyyden
+muuttamiseksi muuta arvoja tiedoston alusta ja käännä uudelleen.
 
-koodipohjan ongelmia:
-index out of bounds ei kaada ohjelmaa tai heitä varoitusta
-väärädimensioisien matriisien kertominen ei kaada ohjelmaa tai heitä varoitusta
-verkkojen tallennusmuoto ei ole järkevä, pitäisi olla ainakin numeropohjainen vaihtoehto
-
-kommentit puuttuu lähes kaikkialta
-testifunktiot puuttuu kaikkialta?
-paljon turhaa toistoa
-useita hyödyllisiä funktioita puuttuu, esimerkiksi funktioita ulkoisen verkon lukemiseen
+tallentaa simulaatiotulokset jos niin halutaan, tallennussijainti muutettava
+kahdesta paikasta.
 */
 
 
 
-#include "../../GFD/Output/MeshDrawer.hpp"	//for drawing a mesh
 #include "../../GFD/Mesh/BuilderMesh.hpp"	//for building and modifying a mesh
 #include "../../GFD/Mesh/PartMesh.hpp"	//for working with parts of a mesh?
 #include "../../GFD/Types/Types.hpp"	//constants, shorter names for types, type couple
@@ -30,14 +24,14 @@ useita hyödyllisiä funktioita puuttuu, esimerkiksi funktioita ulkoisen verkon 
 using namespace std;
 using namespace gfd;
 
-const uint space_steps = 21; // number of space elements per unit length
-const uint time_steps = 4*space_steps; // number of time elements per unit time
+uint space_steps = 32; // number of space elements per unit length
+const double epsilon = 8.9;
+const double r = 0.2;
+
+uint time_steps = 4*space_steps; // number of time elements per unit time
 const uint T = 200; //simulation time
-const uint grid_type = 1; // 2d grid type: 0 = squares, 1 = triangles, 2 = snubsquare
 const double fm = 0.5;
 const double lev = 3.0/(PI*fm);
-const double epsilon = 5.9;
-const double r = 0.3;
 const double korjaus = 0;
 const bool saveresult = true;
 
@@ -80,7 +74,7 @@ Buffer<uint> getUints(const std::string &str) {
 void line(){
 	std::cout << "---------------"<<endl;
 }
-void loadMesh(PartMesh &mesh, string file, Buffer<double> &boundaryNodes){
+void loadMesh(PartMesh &mesh, string file){
     Text input;
     if(input.load(file)) std::cout << "mesh file loaded succesfully." << endl;
     while(input.hasRow()) {
@@ -102,54 +96,34 @@ void loadMesh(PartMesh &mesh, string file, Buffer<double> &boundaryNodes){
     }
     const uint numFace=getUint(input.getRow(),0);
 	std::cout << numFace << endl;
-	//Buffer<uint> flags(numFace*4);//record edges that are linked to only one face -> boundary edges
-	//flags.fill(0);
     for(uint i=0; i<numFace; i++) {
 		const Buffer<uint> face = getUints(input.getRow());
 		Buffer<uint> edge(face.size());
 		for(uint i=0; i<face.size(); i++){
 			edge[i] = mesh.addEdge(face[i%face.size()]-1, face[(i+1)%face.size()]-1);
-			//flags[edge[i]]++;
 		}
 		mesh.addFace(edge);
 	}
-	/*
-	flags.resize(mesh.getEdgeSize());
-	for(uint i=0; i<flags.size();i++){
-		flags[i]=flags[i]%2;//flags[i]=1 iff edge number i is on the boundary
-	}
-	
-	//for(uint i=0;i<flags.size();i++) cout << flags[i] << " ";
-	Buffer<double> flagsV(mesh.getNodeSize());
-	flagsV.fill(0.0);
-	for(uint i=0; i<flags.size();i++){
-		if(flags[i]==1){//flags[i]=1 iff node number i is on the boundary
-			flagsV[mesh.getEdgeNodes(i)[0]]=1.0;
-			flagsV[mesh.getEdgeNodes(i)[1]]=1.0;
-			//mesh.setNodeFlag(mesh.getEdgeNodes(i)[0],1);
-			//mesh.setNodeFlag(mesh.getEdgeNodes(i)[1],1);
-		}
-	}
-	//for(uint i=0;i<flagsV.size();i++) cout << flagsV[i] << " ";
-	boundaryNodes=flagsV;
-	*/
     if(input.getRow().substr(0, 9).compare("$EndFaces") == 0) std::cout << "Faces and edges read succesfully." << endl;
 }
 
 int main(int argc, const char* argv[]) {
 	initMPI();
 
-	auto starttime = chrono::system_clock::now();
-
     PartMesh mesh(0, 1, 2);
-	Buffer<double> boundaryNodes;
     if(argc==1){
-        cout << "give input mesh" << endl;
+        std::cout << "give input mesh" << endl;
 		return 0;
     }
-    else{
-        loadMesh(mesh,argv[1],boundaryNodes);
+    if(argc==2){
+        loadMesh(mesh,argv[1]);
     }
+	if(argc==3){
+		loadMesh(mesh,argv[1]);
+		space_steps=getUint(argv[2],1);
+		time_steps = 4*space_steps;
+		std::cout << argv[2];
+	}
 
 	// initialize matrices and vectors
 	Dec dec(mesh, 0,  mesh.getDimension());
@@ -160,8 +134,8 @@ int main(int argc, const char* argv[]) {
     Diagonal<double> h0i;
     h0i=dec.integrateHodge(HodgeUnit1, 0, fg_dual0, h0i);
 
-    const double dt = 1.0 / double(time_steps);
     const double dx = 1.0 / double(space_steps);
+	const double dt = 1.0 / double(time_steps);
 
     Buffer<double> holes(mesh.getNodeSize());
     Buffer<double> s_m(mesh.getNodeSize());
@@ -175,25 +149,24 @@ int main(int argc, const char* argv[]) {
         yn=mesh.getNodePosition2(i).y;
         if(xn<eps){
             source.push_back(i);
-            //cout << xn << ", " << yn << endl;
             s_m[i]=1.0;
-            if(yn<eps | yn > 1-eps) s_m[i]=.5;
+            if((yn<eps) | (yn > 1-eps)) s_m[i]=.5;
         }
         if(xn>10-eps){
             s_m[i]=1.0;
-            if(yn<eps | yn > 1-eps) s_m[i]=.5;
+            if((yn<eps) | (yn > 1-eps)) s_m[i]=.5;
         }
         if(abs(xn-1)+abs(yn-0.5)<0.8/space_steps){
             tp=i;
-            cout << "Tarkastelupiste 1: " << xn << ", " << yn << endl;
+            std::cout << "Tarkastelupiste 1: " << xn << ", " << yn << endl;
         }
         if(abs(xn-9)+abs(yn-0.5)<0.8/space_steps){
             tpr=i;
-            cout << "Tarkastelupiste 2: " << xn << ", " << yn << endl;
+            std::cout << "Tarkastelupiste 2: " << xn << ", " << yn << endl;
         } 
 		xm=fmod(xn,1.0)-0.5;
 		ym=fmod(yn,1.0)-0.5;
-		if((xn>2)&(xn<8)&(xm*xm+ym*ym)<(r*r+korjaus)) holes[i]=1.0/epsilon;
+		if((xn>2)&(xn<8)&((xm*xm+ym*ym)<(r*r+korjaus))) holes[i]=1.0/epsilon;
 		else holes[i]=1;
 	}
 	Diagonal<double> P2(holes,0.0);
@@ -229,10 +202,11 @@ int main(int argc, const char* argv[]) {
         solt[int(4*i+1.1)]=e.m_val[tpr];
 		solt[int(4*i+2.1)]=e2.m_val[tp];
         solt[int(4*i+3.1)]=e2.m_val[tpr];
-		if(int(i)%time_steps==0)cout << '*';
+		if(int(i)%time_steps==0)std::cout << '*';
     }
-	cout << endl;
+	std::cout << endl;
 	ofstream myfile;
+	/*
 	myfile << setprecision(14);
 	myfile.open ("solutions.txt");
 	uint n_n=mesh.getNodeSize();
@@ -240,15 +214,15 @@ int main(int argc, const char* argv[]) {
 		myfile << e.m_val[i] << endl;
 	}
 	myfile.close();
-	
+	*/
     double dualsum=0;
     double holearea=0;
     for(uint i=0;i<mesh.getNodeSize();i++){
 		dualsum+=1.0/h0i.getValue(i);
         if(holes[i]!=1) holearea+=1.0/h0i.getValue(i);
 	}
-    cout << "sum of dual areas: " << dualsum << " Should be: " << 10 << endl;
-	cout << "area of holes: " << holearea/6.0 << " Should be: " << PI*r*r << " difference: " << abs(PI*r*r-holearea/6.0) << endl;
+    std::cout << "sum of dual areas: " << dualsum << " Should be: " << 10 << endl;
+	std::cout << "radius of holes: " << sqrt(holearea/(6.0*PI)) << " Should be: " << r << " difference: " << abs(PI*r*r-holearea/6.0) << endl;
 
 	if(saveresult){
 		srand((unsigned)time(0)*1942302);
@@ -292,5 +266,5 @@ int main(int argc, const char* argv[]) {
 		myfile.close();
 	}
 
-	gfd::finalizeMPI();
+	finalizeMPI();
 }
