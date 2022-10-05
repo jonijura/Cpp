4a079710099d534235d17948087b7ed61e5c703b
Räty <jonijura@jyu.fi>
2022-10-05 15:47:04 +0300

    mpi piiloon, unitcube makefile windows edition

--- GFD/Types/MpiEasy.cpp
+++ GFD/Types/MpiEasy.cpp
@@ -1,5 +1,5 @@
 #include "MpiEasy.hpp"
-#include <mpi.h>
+//#include <mpi.h>
 
 namespace gfd
 {
@@ -8,40 +8,42 @@ const uint MAXMPISIZE = 4000;
 
 void initMPI()
 {
-	MPI_Init(NULL, NULL);
+	//MPI_Init(NULL, NULL);
 }
 
 void finalizeMPI()
 {
-	MPI_Finalize();
+	//MPI_Finalize();
 }
 
 uint getMPIrank()
 {
-	uint rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, (int*)&rank);
-	return rank;
+	// uint rank;
+	// MPI_Comm_rank(MPI_COMM_WORLD, (int*)&rank);
+	// return rank;
+	return 0;
 }
 
 uint getMPIranks()
 {
-	uint ranks;
-	MPI_Comm_size(MPI_COMM_WORLD, (int*)&ranks);
-	return ranks;
+	// uint ranks;
+	// MPI_Comm_size(MPI_COMM_WORLD, (int*)&ranks);
+	// return ranks;
+	return 1;
 }
 
 void sendMPI(void *data, const uint size, const uint rank, const uint tag)
 {
-	uint j;
-	for(j=0; j+MAXMPISIZE<size; j+=MAXMPISIZE) MPI_Send(&((char *)data)[j], MAXMPISIZE, MPI_CHAR, rank, tag, MPI_COMM_WORLD);
-	MPI_Send(&((char *)data)[j], size - j, MPI_CHAR, rank, tag, MPI_COMM_WORLD);
+	// uint j;
+	// for(j=0; j+MAXMPISIZE<size; j+=MAXMPISIZE) MPI_Send(&((char *)data)[j], MAXMPISIZE, MPI_CHAR, rank, tag, MPI_COMM_WORLD);
+	// MPI_Send(&((char *)data)[j], size - j, MPI_CHAR, rank, tag, MPI_COMM_WORLD);
 }
 
 void recvMPI(void *data, const uint size, const uint rank, const uint tag)
 {
-	uint j;
-	for(j=0; j+MAXMPISIZE<size; j+=MAXMPISIZE) MPI_Recv(&((char *)data)[j], MAXMPISIZE, MPI_CHAR, rank, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	MPI_Recv(&((char *)data)[j], size - j, MPI_CHAR, rank, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	// uint j;
+	// for(j=0; j+MAXMPISIZE<size; j+=MAXMPISIZE) MPI_Recv(&((char *)data)[j], MAXMPISIZE, MPI_CHAR, rank, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	// MPI_Recv(&((char *)data)[j], size - j, MPI_CHAR, rank, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 }
 
 bool orMPI(const bool condition) {
@@ -57,82 +59,82 @@ bool andMPI(const bool condition) {
 
 void sumMPI(uint *sum, const uint size)
 {
-	uint *send = new uint[size];
-	memcpy(send, sum, size * sizeof(uint));
-	MPI_Reduce(send, sum, size, MPI_UNSIGNED, MPI_SUM, 0, MPI_COMM_WORLD);
-	MPI_Bcast(sum, size, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
-	delete[] send;
+	// uint *send = new uint[size];
+	// memcpy(send, sum, size * sizeof(uint));
+	// MPI_Reduce(send, sum, size, MPI_UNSIGNED, MPI_SUM, 0, MPI_COMM_WORLD);
+	// MPI_Bcast(sum, size, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+	// delete[] send;
 }
 void sumMPI(ullong *sum, const uint size)
 {
-	ullong *send = new ullong[size];
-	memcpy(send, sum, size * sizeof(ullong));
-	MPI_Reduce(send, sum, size, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-	MPI_Bcast(sum, size, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
-	delete[] send;
+	// ullong *send = new ullong[size];
+	// memcpy(send, sum, size * sizeof(ullong));
+	// MPI_Reduce(send, sum, size, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+	// MPI_Bcast(sum, size, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
+	// delete[] send;
 }
 void sumMPI(double *sum, const uint size)
 {
-	double *send = new double[size];
-	memcpy(send, sum, size * sizeof(double));
-	MPI_Reduce(send, sum, size, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-	MPI_Bcast(sum, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	delete[] send;
+	// double *send = new double[size];
+	// memcpy(send, sum, size * sizeof(double));
+	// MPI_Reduce(send, sum, size, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	// MPI_Bcast(sum, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	// delete[] send;
 }
 
 void minMPI(uint *sum, const uint size)
 {
-	uint *send = new uint[size];
-	memcpy(send, sum, size * sizeof(uint));
-	MPI_Reduce(send, sum, size, MPI_UNSIGNED, MPI_MIN, 0, MPI_COMM_WORLD);
-	MPI_Bcast(sum, size, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
-	delete[] send;
+	// uint *send = new uint[size];
+	// memcpy(send, sum, size * sizeof(uint));
+	// MPI_Reduce(send, sum, size, MPI_UNSIGNED, MPI_MIN, 0, MPI_COMM_WORLD);
+	// MPI_Bcast(sum, size, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+	// delete[] send;
 }
 void minMPI(ullong *sum, const uint size)
 {
-	ullong *send = new ullong[size];
-	memcpy(send, sum, size * sizeof(ullong));
-	MPI_Reduce(send, sum, size, MPI_UNSIGNED_LONG_LONG, MPI_MIN, 0, MPI_COMM_WORLD);
-	MPI_Bcast(sum, size, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
-	delete[] send;
+	// ullong *send = new ullong[size];
+	// memcpy(send, sum, size * sizeof(ullong));
+	// MPI_Reduce(send, sum, size, MPI_UNSIGNED_LONG_LONG, MPI_MIN, 0, MPI_COMM_WORLD);
+	// MPI_Bcast(sum, size, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
+	// delete[] send;
 }
 void minMPI(double *sum, const uint size)
 {
-	double *send = new double[size];
-	memcpy(send, sum, size * sizeof(double));
-	MPI_Reduce(send, sum, size, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
-	MPI_Bcast(sum, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	delete[] send;
+	// double *send = new double[size];
+	// memcpy(send, sum, size * sizeof(double));
+	// MPI_Reduce(send, sum, size, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
+	// MPI_Bcast(sum, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	// delete[] send;
 }
 
 void maxMPI(uint *sum, const uint size)
 {
-	uint *send = new uint[size];
-	memcpy(send, sum, size * sizeof(uint));
-	MPI_Reduce(send, sum, size, MPI_UNSIGNED, MPI_MAX, 0, MPI_COMM_WORLD);
-	MPI_Bcast(sum, size, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
-	delete[] send;
+	// uint *send = new uint[size];
+	// memcpy(send, sum, size * sizeof(uint));
+	// MPI_Reduce(send, sum, size, MPI_UNSIGNED, MPI_MAX, 0, MPI_COMM_WORLD);
+	// MPI_Bcast(sum, size, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+	// delete[] send;
 }
 void maxMPI(ullong *sum, const uint size)
 {
-	ullong *send = new ullong[size];
-	memcpy(send, sum, size * sizeof(ullong));
-	MPI_Reduce(send, sum, size, MPI_UNSIGNED_LONG_LONG, MPI_MAX, 0, MPI_COMM_WORLD);
-	MPI_Bcast(sum, size, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
-	delete[] send;
+	// ullong *send = new ullong[size];
+	// memcpy(send, sum, size * sizeof(ullong));
+	// MPI_Reduce(send, sum, size, MPI_UNSIGNED_LONG_LONG, MPI_MAX, 0, MPI_COMM_WORLD);
+	// MPI_Bcast(sum, size, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
+	// delete[] send;
 }
 void maxMPI(double *sum, const uint size)
 {
-	double *send = new double[size];
-	memcpy(send, sum, size * sizeof(double));
-	MPI_Reduce(send, sum, size, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-	MPI_Bcast(sum, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	delete[] send;
+	// double *send = new double[size];
+	// memcpy(send, sum, size * sizeof(double));
+	// MPI_Reduce(send, sum, size, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+	// MPI_Bcast(sum, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	// delete[] send;
 }
 
 void barrierMPI()
 {
-	MPI_Barrier(MPI_COMM_WORLD);
+	//MPI_Barrier(MPI_COMM_WORLD);
 }
 
 }
